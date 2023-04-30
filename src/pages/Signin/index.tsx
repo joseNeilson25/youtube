588e31cb27354704e67be3825366a3bdac5648b2
gdpnei2002 <joseneilson25@gmail.com>
2023-04-30 00:00:03 -0300

    completo

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,9 +1,32 @@
 import { GoogleLogo } from "phosphor-react";
+import { signInWithPopup, GoogleAuthProvider, User } from "firebase/auth";
+import { auth } from '../../services/firebase';
+import { useState } from "react";
 
 export function Signin(){
+    const [user, setUser] = useState<User>({} as User);
+
+    function signInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+    
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            console.log(result.user);
+            setUser(result.user);
+    
+          }).catch((error) => {
+            console.log(error);
+          });
+      }
     return(
         <>
             <div>
+                <div>
+                    {user.photoURL && <img src={user.photoURL} alt="Foto do usuário" />}
+
+                    <strong>{user.displayName}</strong>
+                    <small>{user.email}</small>
+                </div>
                 <h1>SignIn</h1>
 
                 <span>
@@ -11,7 +34,7 @@ export function Signin(){
                 facilita a vida do usuário permitindo utilizar a aplicação sem fazer cadastrado.
                 </span>
 
-                <button type="button" className="button">
+                <button type="button" onClick={signInWithGoogle}  className="button">
                 <GoogleLogo />
                     SignIn with Google
                 </button>
