4112ea91e0f795147c79cede2ff92809e80e4a65
gdpnei2002 <joseneilson25@gmail.com>
2023-04-30 00:27:05 -0300

    login 100%

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -2,21 +2,23 @@ import { GoogleLogo } from "phosphor-react";
 import { signInWithPopup, GoogleAuthProvider, User } from "firebase/auth";
 import { auth } from '../../services/firebase';
 import { useState } from "react";
+import { useRouter } from 'next/router';
 
 export function Signin(){
     const [user, setUser] = useState<User>({} as User);
+    const router = useRouter();
 
     function signInWithGoogle() {
         const provider = new GoogleAuthProvider();
     
         signInWithPopup(auth, provider)
-          .then((result) => {
-            console.log(result.user);
-            setUser(result.user);
-    
-          }).catch((error) => {
-            console.log(error);
-          });
+        .then((result) => {
+          console.log(result.user);
+          setUser(result.user);
+          router.push('/Dashboard'); // redireciona para a rota '/outra-pagina'
+        }).catch((error) => {
+          console.log(error);
+        });
       }
     return(
         <>
