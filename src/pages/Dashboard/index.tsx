153594cafba1074d2d2d596c439a40848ec56e9b
gdpnei2002 <joseneilson25@gmail.com>
2023-05-02 12:34:45 -0300

    navbar

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import ResponsiveAppBar from '../components/NavBar';
 
 export default function Dashboard() {
   return (
     <div>
+        <ResponsiveAppBar />
         <h1>Dashboard</h1> 
     </div>
   );
