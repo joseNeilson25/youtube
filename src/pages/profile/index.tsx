dd1a22f362de28c56e504fe605c97cadb1915aae
gdpnei2002 <joseneilson25@gmail.com>
2023-05-03 08:32:03 -0300

    enviando fotos + exibindo-as

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,38 @@
 import { useState, useEffect } from "react";
 import { storage } from '../../services/firebase'
-import { listAll, ref, getDownloadURL } from "firebase/storage";
+import { listAll, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 
 export default function Profile() {
   const [imgURLs, setImgURLs] = useState<string[]>([]);
+  const [imgURL, setImgURL] = useState("");
+  const [progressPorcent, setPorgessPorcent] = useState(0);
 
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const file = event.target[0]?.files[0];
+    if (!file) return;
+    
+    const storageRef = ref(storage, `images/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setPorgessPorcent(progress);
+      },
+      (error) => {
+        alert(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImgURL(downloadURL);
+        });
+      }
+    );
+  };
   useEffect(() => {
     const storageRef = ref(storage, "images/");
     listAll(storageRef)
@@ -30,6 +58,14 @@ export default function Profile() {
           <img src={imgURL} alt="Imagem" height={200} />
         ))}
       </header>
+      <header className="App-header">
+          <form onSubmit={handleSubmit}>
+            <input type="file" />
+            <button>Enviar</button>
+          </form>
+          {!imgURL && <p>{progressPorcent}%</p>}
+          {imgURL && <img src={imgURL} alt="Imagem" height={200} />}
+        </header>
     </div>
   );
 }
